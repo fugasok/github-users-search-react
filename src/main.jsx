4d2944350c1b7748c07e65d3165ed68f60fd8598
Tibor <tibor736@hotmail.com>
2023-10-12 00:25:29 +0300

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { GithubProvider } from './context/context'
+import { Auth0Provider } from '@auth0/auth0-react'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+)
+
